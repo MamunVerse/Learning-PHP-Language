166c253cc5ce8b9b881c677cb377047e55fe33cf
Mamunur Rashid <49125977+MamunVerse@users.noreply.github.com>
2022-10-05 06:35:51 +0600

    Learn Associative Array

---
+++ array/associativeArray.php
@@ -0,0 +1,13 @@
+<!-- 
+ If we define an array of data with a key this array is called an associative array.
+-->
+
+<?php
+
+$students = [
+    '12' => 'Mamun',
+    '13' => 'Sihab',
+    '14' => 'Rony',
+];
+
+echo $students[14];
